96df0fdc95e7b44671cae2d2cfdcd361c1ee0b66
Amostubal <robertearlgriffin@gmail.com>
2017-10-21 09:05:36 -0500

    Update skill-change.lua
    Fixes a number of small issues:
    1. rating now increases if exp exceeds the exp needed for the rating.
    2. -verbose option - turns on and off the little messages for DFHack that appear all over the script 
    3. -max value option - allows the user to state what is the max rating the user of this script will allow it to change it to (ignored if not set)
    4. -reduce yes/no option - (default answer is no)
    4a. if reduce.yes and unit old skill rating > max then the skill rating will get reduced to max at end of script.
    4b. if reduce.no and unit old skill rating => max then the script will stop and return a verbose message that no change occurs.

--- modtools/skill-change.lua
+++ modtools/skill-change.lua
@@ -1,8 +1,9 @@
 -- Sets or modifies a skill of a unit
 --author expwnent
 --based on skillChange.lua by Putnam
---TODO: update skill level once experience increases/decreases
 --TODO: skill rust?
+-- amostubal added max, reduce, and verbose options; additionally update rating
+-- support for when exp exceeds needed amount.
 local help = [====[
 
 modtools/skill-change
@@ -15,6 +16,14 @@ Sets or modifies a skill of a unit.  Args:
                     direct experience, or experience levels?
 :-unit id:          id of the target unit
 :-value amount:     how much to set/add
+:-max amount:       the highest rating that this should not exceed. If not
+                    present the rating will not stop at any level.
+:-reduce (yes/no):  are we reducing units who are over max? default is no.
+                    if a unit is over max rating before this is ran and it
+                    set to no, than it will ignore the function completely
+                    otherwise if its set to yes, it will process through
+                    and reduce the units skill rating to the max mark.
+:-verbose:          adds extra information to the DF Console, for testing.
 
 ]====]
 local utils = require 'utils'
@@ -25,17 +34,25 @@ validArgs = validArgs or utils.invert({
  'mode',
  'value',
  'granularity',
- 'unit'
+ 'unit',
+ 'max',
+ 'reduce',
+ 'verbose'
 })
 
 mode = mode or utils.invert({
  'add',
- 'set',
+ 'set'
+})
+
+reduce = reduce or utils.invert({
+ 'yes',
+ 'no'
 })
 
 granularity = granularity or utils.invert({
  'experience',
- 'level',
+ 'level'
 })
 
 local args = utils.processArgs({...}, validArgs)
@@ -49,17 +66,23 @@ if not args.unit or not tonumber(args.unit) or not df.unit.find(tonumber(args.un
  error 'Invalid unit.'
 end
 args.unit = df.unit.find(tonumber(args.unit))
-
 args.skill = df.job_skill[args.skill]
 args.mode = mode[args.mode or 'set']
+args.reduce = reduce[args.reduce or 'no']
 args.granularity = granularity[args.granularity or 'level']
-args.value = tonumber(args.value)
 
 if not args.skill then
  error('invalid skill')
 end
+
 if not args.value then
  error('invalid value')
+else
+ args.value = tonumber(args.value)
+end
+
+if args.max then
+ args.max = tonumber(args.max)
 end
 
 local skill
@@ -75,14 +98,26 @@ if not skill then
  utils.insert_sorted(args.unit.status.current_soul.skills,skill,'id')
 end
 
-print('old: ' .. skill.rating .. ': ' .. skill.experience)
+if args.reduce == reduce.no and args.max then
+ if skill.rating >= args.max then 
+  if args.verbose then
+   print('unit exceeds max rating and reduce setting is set to no. No Skill Change')
+  end
+  return
+ end
+end
+
+if args.verbose then
+ print('old: ' .. skill.rating .. ': ' .. skill.experience)
+end
+
 if args.granularity == granularity.experience then
  if args.mode == mode.set then
   skill.experience = args.value
  elseif args.mode == mode.add then
   skill.experience = skill.experience + args.value
  else
-  error 'bad mode'
+  error('bad mode')
  end
 elseif args.granularity == granularity.level then
  if args.mode == mode.set then
@@ -90,11 +125,27 @@ elseif args.granularity == granularity.level then
  elseif args.mode == mode.add then
   skill.rating = args.value + skill.rating
  else
-  error 'bad mode'
+  error('bad mode')
  end
 else
- error 'bad granularity'
+ error('bad granularity')
 end
 
-print('new: ' .. skill.rating .. ': ' .. skill.experience)
+-- encoding to increase rating since otherwise it wont until they use the skill
+while skill.experience >= ( ( ( skill.rating + 1 ) * 100 ) + 400 ) do
+ skill.rating = skill.rating + 1 
+ skill.experience = skill.experience - ( ( skill.rating * 100 ) + 400 )
+end
 
+-- Check for going over args.max we don't check for the reduce option here,
+-- Becauuse only being over max pre upgrading skill is an issue.
+if args.max then
+ if skill.rating >=args.max then
+  skill.rating = args.max
+  skill.experience = 0
+ end
+end
+
+if args.verbose then
+ print('new: ' .. skill.rating .. ': ' .. skill.experience)
+end
